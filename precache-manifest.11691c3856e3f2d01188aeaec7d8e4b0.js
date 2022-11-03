e7082c13d0989ecf3d6e126261cebd79f325ded2
TeonaZav <teona.zavrashvili@gmail.com>
2022-11-03 22:05:51 +0400

    Updates

---
+++ precache-manifest.11691c3856e3f2d01188aeaec7d8e4b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4165f37ec3e38847cdbe",
+    "url": "/restaurant-app-react/static/css/main.58d6982c.chunk.css"
+  },
+  {
+    "revision": "4165f37ec3e38847cdbe",
+    "url": "/restaurant-app-react/static/js/main.4165f37e.chunk.js"
+  },
+  {
+    "revision": "23bd05b30d6f59093987",
+    "url": "/restaurant-app-react/static/js/1.23bd05b3.chunk.js"
+  },
+  {
+    "revision": "2c764034644ffee9551c",
+    "url": "/restaurant-app-react/static/js/runtime~main.2c764034.js"
+  },
+  {
+    "revision": "a4e1652251a41412ea635b98bf73b2f6",
+    "url": "/restaurant-app-react/static/media/adjaruli.a4e16522.jpg"
+  },
+  {
+    "revision": "37a180d295945fcfca850e0854bd652e",
+    "url": "/restaurant-app-react/static/media/badrijani.37a180d2.jpg"
+  },
+  {
+    "revision": "01475108c793c1c7ba87bc76b8491871",
+    "url": "/restaurant-app-react/static/media/lobio.01475108.jpg"
+  },
+  {
+    "revision": "6b0f332dbc8e42e966c8ba5c91570b29",
+    "url": "/restaurant-app-react/static/media/pkhali.6b0f332d.jpg"
+  },
+  {
+    "revision": "769f3d3b4436ba4ce10becb3abec39ec",
+    "url": "/restaurant-app-react/static/media/chakapuli.769f3d3b.jpg"
+  },
+  {
+    "revision": "4cfd371d287840b9e535b3c4f56018b4",
+    "url": "/restaurant-app-react/static/media/khinkali.4cfd371d.jpg"
+  },
+  {
+    "revision": "88e0b7b8c80198ba8dab1c40e0d1effc",
+    "url": "/restaurant-app-react/index.html"
+  }
+];
